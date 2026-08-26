7082dea4f432039c392930902e9b3d494caf36fe
NateWNook <Nate@Nook-inc.com>
2026-08-26 12:56:31 -0500

    Add files via upload

--- Candescent TEST.sql
+++ Candescent TEST.sql
@@ -1,2 +1,11 @@
+-- A) (re)create the clean file, output visible
 EXEC xp_cmdshell 'powershell -NoProfile -ExecutionPolicy Bypass -Command "Import-Csv ''\\pd100hubspt01.fcfcu.local\d$\Ancillary_Data_files\Candescent\Candescent-UserData.csv'' -Encoding UTF8 | ConvertTo-Csv -NoTypeInformation -Delimiter ''|'' | ForEach-Object { $_ -replace ([char]34),'''' } | Set-Content ''\\pd100hubspt01.fcfcu.local\d$\Ancillary_Data_files\Candescent\Candescent-clean.psv'' -Encoding UTF8"';
-EXEC xp_cmdshell 'dir "\\pd100hubspt01.fcfcu.local\d$\Ancillary_Data_files\Candescent"';
+
+-- B) file size
+EXEC xp_cmdshell 'dir "\\pd100hubspt01.fcfcu.local\d$\Ancillary_Data_files\Candescent\Candescent-clean.psv"';
+
+-- C) first 3 lines (should be a header line, then pipe-delimited data)
+EXEC xp_cmdshell 'powershell -NoProfile -Command "Get-Content ''\\pd100hubspt01.fcfcu.local\d$\Ancillary_Data_files\Candescent\Candescent-clean.psv'' -TotalCount 3"';
+
+-- D) how many lines does the clean file have?
+EXEC xp_cmdshell 'powershell -NoProfile -Command "(Get-Content ''\\pd100hubspt01.fcfcu.local\d$\Ancillary_Data_files\Candescent\Candescent-clean.psv'').Count"';
