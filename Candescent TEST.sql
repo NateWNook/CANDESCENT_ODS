071301bbdb8f38095d7efee2885195bfd7be2f02
NateWNook <Nate@Nook-inc.com>
2026-08-26 12:49:45 -0500

    Add files via upload

--- Candescent TEST.sql
+++ Candescent TEST.sql
@@ -1,5 +1,2 @@
--- Run THIS proc's pre-clean, output visible:
-EXEC xp_cmdshell 'powershell -NoProfile -ExecutionPolicy Bypass -Command "Import-Csv ''\\pd100hubspt01.fcfcu.local\d$\Ancillary_Data_files\Candescent\Candescent-UserData.csv'' -Encoding UTF8 | ConvertTo-Csv -NoTypeInformation -Delimiter ''|'' | ForEach-Object { $_ -replace ''"'','''' } | Set-Content ''\\pd100hubspt01.fcfcu.local\d$\Ancillary_Data_files\Candescent\Candescent-clean.psv'' -Encoding UTF8"';
-
--- Did the clean file get created?
+EXEC xp_cmdshell 'powershell -NoProfile -ExecutionPolicy Bypass -Command "Import-Csv ''\\pd100hubspt01.fcfcu.local\d$\Ancillary_Data_files\Candescent\Candescent-UserData.csv'' -Encoding UTF8 | ConvertTo-Csv -NoTypeInformation -Delimiter ''|'' | ForEach-Object { $_ -replace ([char]34),'''' } | Set-Content ''\\pd100hubspt01.fcfcu.local\d$\Ancillary_Data_files\Candescent\Candescent-clean.psv'' -Encoding UTF8"';
 EXEC xp_cmdshell 'dir "\\pd100hubspt01.fcfcu.local\d$\Ancillary_Data_files\Candescent"';
